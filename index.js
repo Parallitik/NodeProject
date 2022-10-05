69192c9c38ca67c68a709334138ac058f5ec9b8d
Serjik <serjik@MacBook-Air-Serjik.local>
2022-10-05 12:48:07 +0600

    added timer

--- index.js
+++ index.js
@@ -1,59 +1,75 @@
-import colors from 'colors';
+import EventEmitter from "events";
 
-const firstNum = +process.argv[2];
-const secondNum = +process.argv[3];
-console.log(process.argv);
-const arrPrime = [];
+class MyEmitter extends EventEmitter { }
+const emitter = new MyEmitter();
 
-const isPrime = (num) => {
-    if (num < 2) {
-        return false
-    } else {
-        for (let i = 2; i < num; i++) {
-            if (num % i === 0) {
-                return false;
-            }
+// дата в формате ХХ.ХХ.ХХ.ХХ.ХХХХ - час.мин.день.мес.год
+const finalDate = process.argv[2];
+const parseFinalDate = finalDate.split(".")
+
+const checkDate = (date) => {
+    const splitDate = date.split(".");
+    if (true) {
+        if (splitDate[0] < 0 || splitDate[0] > 24) {
+            console.log("Вы ввели некорректное время");
+            return false;
         }
-        
-            return true;
-        
+        if (splitDate[1] < 0 || splitDate[1] > 59) {
+            console.log("Вы ввели некорректное время");
+            return false;
+        }
+        if (splitDate[2] < 0 || splitDate[2] > 31) {
+            console.log("Вы ввели некорректный день");
+            return false;
+        }
+        if (splitDate[3] < 0 || splitDate[3] > 12) {
+            console.log("Вы ввели некорректный месяц");
+            return false;
+        }
+        if (splitDate[4] < new Date().getFullYear()) {
+            console.log("Вы ввели некорректный год");
+            return false;
+        }
+        console.log(
+            `Таймер установлен до ${splitDate[0]} час. ${splitDate[1]} мин. ${splitDate[2]} день ${splitDate[3]} месяц ${splitDate[4]} год`
+        );
+        emitter.emit("timerUpdate", date);
+        return true;
     }
+};
+
+const timer = () => {
+    let curDate = new Date().getTime(); 
+    let endDate = new Date(`Oct ${parseFinalDate[2]}, ${parseFinalDate[4]} ${parseFinalDate[0]}:${parseFinalDate[1]}:00`).getTime();
+    let diffDate = endDate - curDate;
+    if (diffDate < 0) {
+        emitter.emit("timerFinish");
+    }
+    return diffDate
 }
 
-const numberTrafficLight = (data) => {
-    let count = 1;
-    data.forEach((el) => {
-        switch (count) {
-            case 1: {
-                count++;
-                console.log(colors.red(el));
-                break;
-            }
-            case 2: {
-                count++;
-                console.log(colors.yellow(el));
-                break;
-            }
-            case 3: {
-                count = 1;
-                console.log(colors.green(el));
-                break;
-            }
-        }
-    })
+const converter = () => {
+    let time = timer()
+    let days = Math.floor(time / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    let secs = Math.floor((time % (1000 * 60)) / 1000);
+    return `${days} дней : ${hours} часов : ${mins} минут : ${secs} секунд`
 }
 
-if (isNaN(firstNum) || isNaN(secondNum)) {
-    console.log(colors.red('один из аргументов не является числом'));
-} else {
-    for (let i = firstNum; i < secondNum; i++) {
-        if (isPrime(i)) {
-            arrPrime.push(i);
-        }
-    }
-    if (arrPrime.length === 0) {
-        console.log('Простых чисел в заданном диапазоне нет');
-    } else {
-        numberTrafficLight(arrPrime)
-    }
-}
+const intervalID = setInterval((resolve) =>
+    console.log(converter()), 1000)
+
+const timerUpdate = async () => {
+    await new Promise((resolve) => intervalID);
+};
+
+const timerEnd = () => {
+    clearInterval(intervalID);
+    console.log('КОНЕЦ');
+};
+
+emitter.on("timerUpdate", timerUpdate);
+emitter.on('timerFinish', timerEnd);
+
+checkDate(finalDate);
